5cf592a11b47df67b392da6c8960a32ecc1268d2
Bo Zhang <kainblue@users.noreply.github.com>
2014-12-04 09:02:04 -0500

    Create SwissprotIsoforms.java

---
+++ SwissprotIsoforms.java
@@ -0,0 +1,29 @@
+package com.takeda.tgd.entities;
+
+/**
+ *
+ * @author bozhang1
+ */
+ 
+public class SwissprotIsoforms {
+    
+    public String isoform;
+
+    public void setIsoform(String isoform) {
+        this.isoform = isoform;
+    }
+
+    public void setIsoformLength(String isoformLength) {
+        this.isoformLength = isoformLength;
+    }
+
+    public String getIsoform() {
+        return isoform;
+    }
+
+    public String getIsoformLength() {
+        return isoformLength;
+    }
+    public String isoformLength;
+    
+}
